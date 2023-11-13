5de11a91f1d6ff6cee5a4c90666f33a6dd7a4f64
seth <getchoo@tuta.io>
2023-11-12 20:07:55 -0500

    feat: use all images from teawie-archive

--- build.ts
+++ build.ts
@@ -1,43 +1,62 @@
 import { BuildOptions, build } from "esbuild";
 import { existsSync } from "node:fs";
 import { constants, copyFile, mkdir, readdir, rm } from "node:fs/promises";
-import { join } from "node:path";
+import { basename, join } from "node:path";
 
 const distDir = "dist";
-const contentDir = join(distDir, "static");
-const teawieArchiveDir = "Teawie-Archive/teawie-media/Original Teawies";
+const contentDir = "static";
+const teawieArchiveDir = "Teawie-Archive/teawie-media";
+const teawieContentDir = join(contentDir, "teawie");
 
 const checkAndCreate = async (dir: string) => {
 	if (!existsSync(dir)) {
 		await mkdir(dir, { recursive: true });
 	} else {
-		for (const f of await readdir(dir)) {
-			await rm(join(dir, f), { recursive: true, force: true });
+		for (const f of await readdir(dir, { withFileTypes: true })) {
+			await rm(f.path, { recursive: true, force: true });
 		}
 	}
 };
 
-await checkAndCreate(distDir);
-await checkAndCreate(contentDir);
-await checkAndCreate(join(contentDir, "teawie"));
+const findWies = async () => {
+	// grab directories from archive
+	const wieDirs = (await readdir(teawieArchiveDir, { withFileTypes: true }))
+		.filter((file) => file.isDirectory())
+		.map((file) => join(file.path, file.name));
 
-const wies = (await readdir(teawieArchiveDir)).filter((wie) => {
-	const fileExt = wie.split(".").pop();
-	return !["ini", "txt"].includes(fileExt ?? "");
-});
+	// map over directories until we (probably) only have the images we want and their path
+	const collected = wieDirs.map(async (dir) =>
+		(await readdir(dir, { withFileTypes: true }))
+			.filter(async (file) => {
+				const fileExt = file.name.split(",").pop() ?? "";
+				return file.isFile() && !["ini", "txt"].includes(fileExt);
+			})
+			.map((file) => join(file.path, file.name)),
+	);
+
+	const res = await Promise.all(collected);
+
+	return res.flat();
+};
+
+const wies = await findWies();
+
+const define = {
+	WIES: JSON.stringify(wies.map((wie) => basename(wie).replaceAll(" ", "%20"))),
+	WIE_DIR: JSON.stringify(teawieContentDir),
+};
+
+await checkAndCreate(distDir);
+await checkAndCreate(join(distDir, contentDir));
+await checkAndCreate(join(distDir, teawieContentDir));
 
 for (const f of wies) {
 	await copyFile(
-		join(teawieArchiveDir, f),
-		join(contentDir, "teawie", f),
+		f,
+		join(distDir, teawieContentDir, basename(f)),
 		constants.COPYFILE_FICLONE,
 	);
 }
-
-const define = {
-	WIES: JSON.stringify(wies.map((wie) => wie.replaceAll(" ", "%20"))),
-	WIE_DIR: JSON.stringify("static/teawie"),
-};
 
 const options = {
 	entryPoints: ["src/index.ts"],
